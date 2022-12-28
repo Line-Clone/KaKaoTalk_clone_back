5c706ba10ab33baf4d319c1ed453b470a1683a5c
Sangtriever <dltkdgus1850@gmail.com>
2022-12-28 16:17:38 +0900

    feat : 메세지 db 저장시, MessageType 입력, 입장시 메시지는 db저장 x로 변경

--- src/main/java/com/sparta/lineclone/repository/ChatRepository.java
+++ src/main/java/com/sparta/lineclone/repository/ChatRepository.java
@@ -3,12 +3,13 @@ package com.sparta.lineclone.repository;
 import com.sparta.lineclone.entity.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 
     Optional<Chat> findByRoomId(String roomId);
 
-    boolean findBySender(String nickname);
+    List<Chat> findBySender(String nickname);
 
 }
